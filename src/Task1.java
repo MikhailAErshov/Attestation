c2bf8edf5ba0e303402f0484fd7fb28ea6c1018f
MikhailAErshov <aem.cmd@gmail.com>
2023-02-21 19:56:15 +0300

    solved

--- src/Task1.java
+++ src/Task1.java
@@ -3,5 +3,21 @@
 //Напишите программу, которая поможет по длине забора (введенной пользователем) выяснить,
 //поместится ли на забор такой длины надпись мальчика.
 
+import java.util.Scanner;
+
 public class Task1 {
+    public static void main(String[] args) {
+        int length3Words = 62;
+        int length1Backspace = 12;
+        int lengthTotal = length3Words * 5 + length1Backspace * 3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите длину забора (в см)...");
+        int lengthFense = sc.nextInt();
+        while (lengthFense < lengthTotal) {
+            System.out.println("Длины забора недостаточно, давай увеличим длину");
+            lengthFense = sc.nextInt();
+        }
+        System.out.println("Ок. Длины забора достаточно!");
+        sc.close();
+    }
 }
